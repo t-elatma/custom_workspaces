26d48fe9624be89f9a579906a7a433862b0cafd6
t-elatma <39800358+t-elatma@users.noreply.github.com>
2026-03-26 01:11:38 +0200

    v_0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in custom_app/__init__.py
+# get version from __version__ variable in custom_workspaces/__init__.py
 from custom_workspaces import __version__ as version
 
 setup(
